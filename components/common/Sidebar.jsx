2e787c2e34338c7683f5a20357b0000831d80e2a
sriram200408 <f20221082@pilani.bits-pilani.ac.in>
2024-06-28 13:08:43 +0530

    friday1

--- components/common/Sidebar.jsx
+++ components/common/Sidebar.jsx
@@ -14,54 +14,33 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Image from "next/image";
+
 
 const Sidebar = ({ children }) => {
   const { open } = useContext(MenuContext);
   return (
-    <div className="bg-white  min-h-screen">
+    <div className="bg-white ">
       <MainHeader />
 
       <div className="flex justify-start items-start ">
         <aside className="bg-white rounded-lg absolute inset-y-0 left-0 border border-gray-200 shadow-sm w-2/12">
-          <div className="h-4 w-48 absolute left-16 top-4 ">
-            <img src="https://fervidsmart.com/dist/assets/img/logo/Fervid_Logo_svg.svg"></img>
+          <div className="h-12 w-48 absolute left-16 top-4 ">
+            <Image fill={true} src="https://fervidsmart.com/dist/assets/img/logo/Fervid_Logo_svg.svg"></Image>
           </div>
           <div className="flex justify-start items-center cursor-pointer  rounded-xl absolute top-20 left-10">
             <IoHomeSharp className="mr-2" />
             <Link href="/dashboard">Dashboard</Link>
           </div>
           <div className="absolute top-32 w-48 left-10">
-            <Accordion type="single" collapsible>
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Products</AccordionTrigger>
-                <AccordionContent>
-                  <Link href="/Products/productslist"> Products List</Link>
-                </AccordionContent>
-                <AccordionContent>
-                  <Link href="/Products/addproducts">Add Product</Link>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger>Categories</AccordionTrigger>
-                <AccordionContent>
-                  <a href="https://google.com" className="cursor-pointer">
+          <ul>
+              <li className="cursor-pointer hover:bg-slate-200 hover:focus:bg-slate-200">Product</li> 
+              <li><Link href="/Products/productslist"> Products List</Link></li>
+              <li><Link href="/Products/addproducts">Add Product</Link></li>
+              <li><a href="https://google.com" className="cursor-pointer">
                     Products{" "}
-                  </a>
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-3">
-                <AccordionTrigger>Sales</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-4">
-                <AccordionTrigger>Purchases</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
+                  </a></li>            
+                </ul>
           </div>
         </aside>
         <div className="absolute inset-y-0 right-0 w-5/6 top-24">
--- components/products/AddProducts.jsx
+++ components/products/AddProducts.jsx
@@ -7,33 +7,122 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select";
+  import { Label } from "@/components/ui/label"
+  import { Textarea } from "@/components/ui/textarea"
+  import { Button } from "@/components/ui/button"
+
+
+
 
   export  function AddProducts () {
+    
     return (
         <>
+        <div id="areawrap" className="rounded-xld border-black m-4">
         <div>
-            <h2>Add Product</h2>
+            <h2 className="font-bold ">Add Product</h2>
         </div>
-        <div>
-            <h3>Product Type *</h3>
-            <Select>
-  <SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Product Type" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="standard">Standard</SelectItem>
-    <SelectItem value="combo">Combo</SelectItem>
-    <SelectItem value="digital">Digital</SelectItem>
-    <SelectItem value="service">Service</SelectItem>
-  </SelectContent>
-</Select>
+        <div className="mt-12">
+        <Label className="mb-5">Product</Label>
+
+            <Select className="">
+              <SelectTrigger className="w-full">
+              <SelectValue placeholder="Product Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Exclusive">Exclusive</SelectItem>
+                 <SelectItem value="combo">Inclusive</SelectItem>
+                  <SelectItem value="digital">Digital</SelectItem>
+                  <SelectItem value="service">Service</SelectItem>
+              </SelectContent>
+             </Select>
+        </div>
+        <div className="flex mt-10">
+          <div className="col-span-1 w-6/12 ">
+        <Label >Name</Label>
+            <Input className="mt-1"/>
+            </div>
+            <div className="col-span-1 w-6/12 ml-10">
+            <Label >Code</Label>
+            <Input className="mt-1"/>
+            </div>
+        </div>
+        <div className=" mt-7 flex">
+        <div className=" w-6/12">
+        <Label className="mb-5">Barcode Symbology</Label>
+
+            <Select className="mt-1">
+              <SelectTrigger className="w-full">
+              <SelectValue placeholder="Product Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="CRE-M01">CRE-M01</SelectItem>
+                 <SelectItem value="MOM-S02">MOM-S02</SelectItem>
+                  <SelectItem value="TIS-M01">TIS-M01</SelectItem>
+                  <SelectItem value="SDE-W02">SDE-W02</SelectItem>
+                  <SelectItem value="WAP-P609">WAP-P609</SelectItem>
+                 <SelectItem value="DIS-T01">DIS-T01</SelectItem>
+                  <SelectItem value="LOD-A01">LOD-A01</SelectItem>
+                  <SelectItem value="QOS-I03">QOS-I03</SelectItem>
+              </SelectContent>
+             </Select>
+        </div>
+        <div className=" w-6/12 ml-10">
+        <Label className="mb-5">Category</Label>
+
+            <Select className="mt-1">
+              <SelectTrigger className="w-full">
+              <SelectValue placeholder="Product Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="beauty">Beauty</SelectItem>
+                 <SelectItem value="food">Food</SelectItem>
+                  <SelectItem value="furniture">Furniture</SelectItem>
+                  <SelectItem value="toys">Toys</SelectItem>
+                  <SelectItem value="shoes">Shoes</SelectItem>
+                 <SelectItem value="cosmetics">Cosmetics</SelectItem>
+                  <SelectItem value="grocery">Grocery</SelectItem>
+                  <SelectItem value="sports">Sports</SelectItem>
+              </SelectContent>
+             </Select>
+        </div>
+        </div>
+        <div className="flex mt-7">
+          <div className="col-span-1 w-6/12 ">
+            <Label className="mb-5">Cost</Label>
+            <Input className="mt-1"/>
+            </div>
+            <div className="col-span-1 w-6/12 ml-10">
+            <Label className="mb-5">Price</Label>
+            <Input className="mt-1"/>
+            </div>
+        </div>
+        <div className="mt-12">
+        <Label className="mb-5">Tax Method</Label>
+            <Select className="mt-1">
+              <SelectTrigger className="w-full">
+              <SelectValue placeholder="Tax Method" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="standard">Exclusive</SelectItem>
+                 <SelectItem value="Inclusive">Inclusive</SelectItem>
+              </SelectContent>
+             </Select>
+        </div>
+        <div className="mt-7">
+        <Label className="mb-5" >Quantity</Label>
+            <Input className="mt-1"/>
+        </div>
+        <div className="mt-5">
+          <Label className="mb-5">Product details/Description</Label>
+          <Textarea className="mt-1"/>
+        </div>
+        <div className="flex">
+        <Button variant="outline" className="mt-5 hover:bg-green-500 hover:text-white">Add Product</Button>
+        <Button variant="outline"className="mt-5 ml-5 hover:bg-blue-500 hover:text-white">Reset</Button>
+
 
         </div>
-        <div>
-            <h3>Name *</h3>
-            <Input/>
-            <h3>Code *</h3>
-            <Input/>
         </div>
         </>
     );
